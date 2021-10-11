69dedcf770e9e9b864b41b38284ef447d080ed61
Roeland Lundahl <roeland@imachine.nl>
2021-10-11 16:44:37 +0200

    implements authentication

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers\API;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Auth;
 
 class UsersController extends Controller
 {
-    public function show()
+    public function __construct()
     {
-    
+        $this->middleware('auth:sanctum')->only('show');
+    }
+
+    public function show(Request $request)
+    {
+        return response()->json( $request->user() );
     }
 }
--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,21 +4,57 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Auth;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        $validator = Validator::make( $request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+            'confirm_password' => 'required|same:password'
+        ]);
+
+        if( !$validator->fails() ){
+            $user = User::create([
+                'name' => $request->get('name'),
+                'email' => $request->get('email'),
+                'password' => bcrypt( $request->get('password') ),
+                // 'avatar' => $gravatar->get()
+            ]);
+            
+            return response()->json('', 204);
+        }
+
+        return response()->json([
+            'success' => false,
+            'errors' => $validator->errors()
+        ], 422);
 
     }
 
     public function login(Request $request)
     {
-    
+        if (Auth::attempt([
+            'email' => $request->get('email'),
+            'password' => $request->get('password')
+        ])) {
+            return response()->json('', 204);
+        } else {
+            return response()->json([
+                'error' => 'invalid_credentials'
+            ], 403);
+        }
     }
 
     public function logout(Request $request)
     {
-    
+        Auth::logout();
+        return response()->json('', 204);
     }
 }
